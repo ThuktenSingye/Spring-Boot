48f0cd394e37e6e5964b50d72882ad446c3d6e11
ThuktenSingye <thuktensingye2163@gmail.com>
2023-12-29 16:05:27 +0600

    Simple Native Query

--- user-management/src/main/java/com/sdu/usermanagement/repository/UserRepository.java
+++ user-management/src/main/java/com/sdu/usermanagement/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.sdu.usermanagement.repository;
 import com.sdu.usermanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-    
+    @Modifying
+    @Query(value = "UPDATE user SET Email = :email WHERE User_Id = :userId", nativeQuery = true)
+    void updateUserEmail(@Param("email") String email,@Param("userId") Integer userId);
 }
