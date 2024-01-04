e8cf1c75109a0b35f718c141e6e3fcc4837f105c
ThuktenSingye <thuktensingye2163@gmail.com>
2024-01-04 19:12:41 +0600

    updated User field

--- user-management/src/main/java/com/sdu/usermanagement/model/User.java
+++ user-management/src/main/java/com/sdu/usermanagement/model/User.java
@@ -54,11 +54,11 @@ public class User{
             CascadeType.MERGE
         }
     )
-    @JoinColumn(name = "GenderId")
+    @JoinColumn(name = "Gender_Id")
     private Gender gender;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "AddressId")
+    @JoinColumn(name = "Address_Id")
     private Address address;
 
     @ManyToOne(
@@ -69,11 +69,11 @@ public class User{
             CascadeType.MERGE
         }
     )
-    @JoinColumn(name = "SectionId")
+    @JoinColumn(name = "Section_Id")
     private Section section;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "ImageId")
+    @JoinColumn(name = "Image_Id")
     private ProfileImage profileImage;
 
 }
