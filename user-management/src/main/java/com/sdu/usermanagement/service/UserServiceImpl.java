c5a74d2ba07226ab545a1389d1fede4b96209e28
ThuktenSingye <thuktensingye2163@gmail.com>
2024-01-04 14:27:57 +0600

    included file name generator utility

--- user-management/src/main/java/com/sdu/usermanagement/service/UserServiceImpl.java
+++ user-management/src/main/java/com/sdu/usermanagement/service/UserServiceImpl.java
@@ -1,10 +1,7 @@
 package com.sdu.usermanagement.service;
 import java.io.File;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -231,7 +228,7 @@ public class UserServiceImpl implements UserService{
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     
-
+    }
      /* Method to convert User Entity to User DTO */
     private UserDTO userEntityToDto(User user){
         UserDTO userDTO = new UserDTO();
